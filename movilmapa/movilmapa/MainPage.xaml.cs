af5e6c8cdac87eb4a03a32c87cb993bf1b2bdfeb
unknown <Hashirama1000>
2021-06-18 03:11:42 -0600

    subiendo los primeros archivos

---
+++ movilmapa/movilmapa/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace movilmapa
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtLatitud.Text, out double lat))
+                return;
+            if (!double.TryParse(txtLongitud.Text, out double lng))
+                return;
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+
+                Name = txtNombreUbica.Text,
+                NavigationMode = NavigationMode.None
+
+            });
+        }
+    }
+}
